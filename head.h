a3d9f3c13a37d2747a17fd72eca07707ebb02ef6
Gabriel Aurélio <135907544+igabrielaurelio@users.noreply.github.com>
2023-08-28 23:09:01 -0300

    head.h

---
+++ head.h
@@ -0,0 +1,12 @@
+#ifndef head_h
+#define head_h
+
+int menu();
+void criarcontato();
+void apagacontato();
+void editarnome();
+void editaremail();
+void editartelefone();
+void acessarcontato();
+int tipodeedicao();
+#endif
